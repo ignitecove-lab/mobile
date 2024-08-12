2e51db0bed6fe66e058ac53e5df8b548263b165d
charlesncn <charowcjn@gmail.com>
2024-08-12 11:33:53 +0300

    disable rtl

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { MenuProvider } from "react-native-popup-menu";
 import StackNavigator from "./StackNavigator";
 import { StatusBar } from "expo-status-bar";
+import { I18nManager } from 'react-native';
 import { AuthProvider } from "./useAuth";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+
+I18nManager.forceRTL(false);
+I18nManager.allowRTL(false);
 
 export default function App() {
   return (
